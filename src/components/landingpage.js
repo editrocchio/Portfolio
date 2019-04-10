0817f2ba0c7482cb1c40730d6039f41447c17ec8
Emilio <editrocchio@gmail.com>
2019-04-09 23:53:04 -0700

    worked on landing and projects page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import profile from '../bcit-nov28-2316.png'
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style = {{width: '100%', margin: 'auto'}}>
+        <Grid className='landing-grid'>
+          <Cell col={12}>
+            <img src={profile} alt='profile_pic' className='profile-img'/>
+
+            <div className='banner-text'>
+
+              <h1>BCIT Computer Systems Technology Student</h1>
+              <hr/>
+              <p>Android | Java | JavaScript | Python | HTML/CSS | OracleDB</p>
+
+              <div className="social-links">
+                {/*Linkedin*/}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i class="fab fa-linkedin" aria-hidden="true"></i>
+                </a>
+
+                {/*GitHub*/}
+                <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                  <i class="fab fa-github-square" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-
+import { Tabs, Tab, Grid, Cell } from 'react-mdl';
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+toggleCategories() {
+  if(this.state.activeTab===0) {
+    return(
+      <div><h1>Android</h1></div>
+    )
+  } else if(this.state.activeTab===1) {
+    return(
+      <div><h1>Python</h1></div>
+    )
+  } else if(this.state.activeTab===2) {
+    return(
+      <div><h1>Web</h1></div>
+    )
+  }
+}
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className='category-tabs'>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+          <Tab>Android</Tab>
+          <Tab>Python</Tab>
+          <Tab>Web</Tab>
+        </Tabs>
+        <section className="projects-grid">
+          {this.toggleCategories()}
+        </section>
       </div>
     )
   }
