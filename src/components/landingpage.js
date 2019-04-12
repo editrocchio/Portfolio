cef50e7735b1d144cc6f9a8ea3c6391c799a53cc
Emilio Ditrocchio <editrocchio@gmail.com>
2019-04-11 20:49:02 -0700

    switched to scrollable

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,44 +1,92 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Grid  from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import profile from '../bcit-nov28-2316.png'
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
     color: "white",
     textAlign: "center",
-    margin: "15% 0 0 25%",
+    marginTop: "15%",
     width: "100%",
+    paddingBottom: "50px"
   },
+  about: {
+      margin: "0 auto",
+      paddingTop: "15%",
+  },
+  paper: {
+      color: "black"
+  }
+
 });
+
+
 
 function CenteredGrid(props) {
   const { classes } = props;
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={6}>
-
-        <Grid item xs={6}>
-
+      <Grid container spacing={12}>
+        <Grid item xs={12}>
           <Typography component="h1">
             Emilio Ditrocchio
-
           </Typography>
           <Typography component="h2">
              BCIT Computer Systems Technology Student
            </Typography>
             <Typography variant="subtitle2">
-            Android | Java | JavaScript | Python | HTML/CSS | OracleDB
+            Welcome to my portfolio website
           </Typography>
+        </Grid>
+
+        <Grid item xs={6} className={classes.about}>
+            <Typography component="h1">
+              About Me
+            </Typography>
+
+            <Typography component="h4">
+            Lorem ipsum conubia enim mi primis netus aliquam rhoncus, vestibulum ut iaculis etiam cubilia ut mauris hendrerit sociosqu cubilia etiam mauris.
+            Phasellus augue ad ornare eleifend orci curabitur est ut, massa congue senectus faucibus dictum gravida ante nisl, nullam leo egestas dui est mollis adipiscing.
+
+            </Typography>
+        </Grid>
+
+        <Grid item xs={8} className={classes.projects}>
+            <Paper className={classes.paper} elevation={1}>
+            <Typography component="h1">
+              Projects
+            </Typography>
+
+            <AppBar position="static">
+          <Tabs >
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+          </Tabs>
+        </AppBar>
+
+          </Paper>
+
 
         </Grid>
 
-      </Grid>
+    </Grid>
+
     </div>
+
+
+
+
   );
 }
 
